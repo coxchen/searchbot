12c2f9be96226047b427cfdff1e4d9f34f62a8f8
Cox Chen <coxchen@gmail.com>
2015-04-27 18:42:32 +0800

    charting with ES aggregation result

--- src/cljs/searchbot/core.cljs
+++ src/cljs/searchbot/core.cljs
@@ -7,29 +7,39 @@
             [cljs-http.client :as http]
             [goog.events :as events]
             [cljs.core.async :refer [put! <! >! chan timeout]]
-            [searchbot.widgets :refer [header agg-summary agg-table chart]]))
+            [searchbot.widgets :refer [header agg-summary agg-table chart es-chart aggregator]]))
 
 (defonce app-state (atom {:header-text "AVC realtime aggregation"
                           :avc-count 0
-                          :agg {}
-                          :chart {:div {:width "90%" :height 300}
-                                  :data [{:value 240000 :timestamp "2014-01-01"}
-                                         {:value 260000 :timestamp "2014-02-01"}
-                                         {:value 290000 :timestamp "2014-03-01"}
-                                         {:value 70000  :timestamp "2014-04-01"}
-                                         {:value 100000 :timestamp "2014-05-01"}
-                                         {:value 120000 :timestamp "2014-06-01"}
-                                         {:value 240000 :timestamp "2014-07-01"}
-                                         {:value 220000 :timestamp "2014-08-01"}
-                                         {:value 360000 :timestamp "2014-09-01"}
-                                         {:value 260000 :timestamp "2014-10-01"}
-                                         {:value 250000 :timestamp "2014-11-01"}
-                                         {:value 190000 :timestamp "2014-12-01"}]}
+                          :agg {:div {:width "90%" :height 300}}
                           }))
 
 (defcomponent my-app [app owner]
   (render [this] (html [:div
                         (om/build header app)
+                        (om/build aggregator app
+                                  {:opts {:agg-key "SSID AGG"
+                                          :body {:aggs {:ssids
+                                                        {:terms {:field "ssid" :order {:sum_usage "desc"}}
+                                                         :aggs {:sum_usage {:sum {:field "usage"}}
+                                                                :client_count
+                                                                {:cardinality {:field "client_mac.hash"
+                                                                               :precision_threshold 50000}}}}}}}})
+                        (om/build aggregator app
+                                  {:opts {:agg-key "APP AGG"
+                                          :body {:aggs {:apps
+                                                        {:terms {:field "app" :order {:sum_usage "desc"}}
+                                                         :aggs {:sum_usage {:sum {:field "usage"}}
+                                                                :sum_up {:sum {:field "up"}}
+                                                                :sum_down {:sum {:field "down"}}}}}}}})
+                        (om/build aggregator app
+                                  {:opts {:agg-key "TIME AGG"
+                                          :body {:aggs {:traffic_over_time
+                                                        {:date_histogram
+                                                         {:field "timestamp" :interval "1h"
+                                                          :format "MM-dd kk:mm" :post_zone "+08:00"}
+                                                         :aggs {:sum_up {:sum {:field "up"}}
+                                                                :sum_down {:sum {:field "down"}}}}}}}})
                         [:.row [:.col-lg-4 (om/build agg-summary app)]]
                         [:.row
                          [:.col-lg-3
@@ -38,56 +48,57 @@
                                             :agg-top "ssids"
                                             :header [{:label "SSID" :agg :key}
                                                      {:label "Client Count" :agg :client_count}
-                                                     {:label "Usage" :agg :sum_usage}]
-                                            :body {:query {:filtered {:query {:match_all {}}}}
-                                                   :aggs {:ssids
-                                                          {:terms {:field "ssid" :order {:sum_usage "desc"}}
-                                                           :aggs {:sum_usage {:sum {:field "usage"}}
-                                                                  :client_count
-                                                                  {:cardinality {:field "client_mac.hash"
-                                                                                 :precision_threshold 50000}}}}}}}})]
-                         [:.col-lg-4
+                                                     {:label "Usage" :agg :sum_usage}]}})]
+                         [:.col-lg-3
                           (om/build agg-table app
                                     {:opts {:agg-key "APP AGG"
                                             :agg-top "apps"
                                             :header [{:label "Application" :agg :key}
                                                      {:label "Usage" :agg :sum_usage}
                                                      {:label "UpLink" :agg :sum_up}
-                                                     {:label "DownLink" :agg :sum_down}]
-                                            :body {:aggs {:apps
-                                                          {:terms {:field "app" :order {:sum_usage "desc"}}
-                                                           :aggs {:sum_usage {:sum {:field "usage"}}
-                                                                  :sum_up {:sum {:field "up"}}
-                                                                  :sum_down {:sum {:field "down"}}}}}}}})]
-
+                                                     {:label "DownLink" :agg :sum_down}]}})]
+                         [:.col-lg-3
+                          (om/build agg-table app
+                                    {:opts {:agg-key "TIME AGG"
+                                            :agg-top "traffic_over_time"
+                                            :header [{:label "TIME" :agg :key_as_string}
+                                                     {:label "UpLink" :agg :sum_up}
+                                                     {:label "DownLink" :agg :sum_down}]}})]
                          ]
                         [:.row
                          [:.col-lg-3
-                          (om/build chart (:chart app)
-                                    {:opts {:id "bar"
-                                            :draw-fn :draw-bar
-                                            :chart {:bounds {:x "5%" :y "15%" :width "80%" :height "80%"}
-                                                    :plot js/dimple.plot.bar
-                                                    :x-axis "timestamp"
-                                                    :y-axis "value"}}})
-                          ]
-                         [:.col-lg-3
-                          (om/build chart (:chart app)
-                                    {:opts {:id "pie"
+                          (om/build es-chart (:agg app)
+                                    {:opts {:id "ssid_pie"
+                                            :agg-key "SSID AGG"
+                                            :agg-top "ssids"
+                                            :agg-view :sum_usage
                                             :chart {:bounds {:x "5%" :y "15%" :width "80%" :height "80%"}
                                                     :plot js/dimple.plot.pie
-                                                    :p-axis "value"
-                                                    :c-axis "timestamp"}
+                                                    :p-axis "sum_usage"
+                                                    :c-axis "key"}
                                             :draw-fn :draw-ring}})]
                          [:.col-lg-3
-                          (om/build chart (:chart app)
-                                    {:opts {:id "line"
-                                            :draw-fn :draw-bar
+                          (om/build es-chart (:agg app)
+                                    {:opts {:id "app_pie"
+                                            :agg-key "APP AGG"
+                                            :agg-top "apps"
+                                            :agg-view :sum_usage
+                                            :chart {:bounds {:x "5%" :y "15%" :width "80%" :height "80%"}
+                                                    :plot js/dimple.plot.pie
+                                                    :p-axis "sum_usage"
+                                                    :c-axis "key"}
+                                            :draw-fn :draw-ring}})]
+                         [:.col-lg-3
+                          (om/build es-chart (:agg app)
+                                    {:opts {:id "time_line"
+                                            :agg-key "TIME AGG"
+                                            :agg-top "traffic_over_time"
+                                            :agg-view :sum_up
+                                            :draw-fn :draw-line
                                             :chart {:bounds {:x "10%" :y "5%" :width "80%" :height "70%"}
                                                     :plot js/dimple.plot.line
-                                                    :x-axis "timestamp"
-                                                    :y-axis "value"}}})
-                          ]
+                                                    :x-axis "key_as_string"
+                                                    :y-axis "sum_up"}}})]
                          ]
                         ])))
 
--- src/cljs/searchbot/widgets.cljs
+++ src/cljs/searchbot/widgets.cljs
@@ -39,10 +39,8 @@
 
 (defcomponent header [app owner opts]
   (will-mount [_]
-;;               (om/transact! app :avc-count (fn [_] 0))
               (go (while true
                     (let [avcCount (<! (avc-count))]
-                      (.log js/console (pr-str "[avc count]" avcCount))
                       (om/update! app [:avc-count] avcCount))
                     (<! (timeout poll-interval)))))
   (render [_]
@@ -56,24 +54,13 @@
                           [:th {:align "right"} "Records aggregated"]
                           [:th {:align "right"} "Time (ms)"]]]
                         [:tbody
-                         (for [aggKey (-> app :agg keys)]
+                         (for [aggKey (filter #(not= :div %) (-> app :agg keys))]
                            [:tr
                             [:td [:span (name aggKey)]]
                             [:td (-> app :agg aggKey :hits :total str commify)]
                             [:td (-> app :agg aggKey :took str commify)]])]])))
 
 (defcomponent agg-table [app owner opts]
-  (will-mount [_]
-              (go (while true
-                    (let [agg-key (-> opts :agg-key keyword)
-                          agg-resp (<! (avc-agg (:body opts)))]
-;;                       (.log js/console (pr-str agg-key))
-;;                       (.log js/console (pr-str agg-resp))
-                      (om/update! app [:agg agg-key] agg-resp)
-;;                       (.log js/console (pr-str "agg" (@app :agg)))
-;;                       (.log js/console (pr-str "agg keys" (-> @app :agg agg-key :aggregations keys)))
-                      )
-                    (<! (timeout poll-interval)))))
   (render [this] (html [:.panel.panel-info
                         [:.panel-heading (-> opts :agg-key name)]
                         [:.panel-body
@@ -90,6 +77,26 @@
                                 [:td (->> opts :header first :agg (get x))]
                                 (for [d (->> opts :header rest)]
                                   [:td (-> x (get-in [(:agg d) :value]) str commify)])]))]]]])))
+
+(defcomponent aggregator [app owner opts]
+  (will-mount [_]
+              (go (while true
+                    (let [agg-key (-> opts :agg-key keyword)
+                          agg-resp (<! (avc-agg (merge (:body opts)
+                                                       {:query
+                                                        {:filtered
+                                                         {:query {:match_all {}}
+                                                          :filter {:range {:timestamp {:gte "now-12h" :lte "now"}}}}}})))]
+                      (if (= agg-key (keyword "TIME AGG"))
+                        (do
+                          (.log js/console (pr-str agg-key))
+                          (.log js/console (pr-str agg-resp))))
+                      (om/update! app [:agg agg-key] agg-resp)
+;;                       (.log js/console (pr-str "agg" (@app :agg)))
+;;                       (.log js/console (pr-str "agg keys" (-> @app :agg agg-key :aggregations keys)))
+                      )
+                    (<! (timeout poll-interval)))))
+  (render [_] (html [:.hide-agg "Aggregator:" (-> opts :agg-key)])))
 
 ;;;;;;;;;;
 ;; charts
@@ -150,7 +157,7 @@
 
 (defn chart-fn [fn-name]
   (case fn-name
-    :draw-bar draw-bar
+    :draw-bar  draw-bar
     :draw-ring draw-ring
     :draw-line draw-line
     nil))
@@ -168,7 +175,6 @@
             (html [:div {:id id :width width :height height}])))
   (did-mount [_]
              (when-let [data (seq (:data cursor))]
-               (.log js/console (pr-str "[draw-fn]" draw-fn "[fn?]" (fn? draw-fn)))
                ((chart-fn draw-fn) data (:div cursor) opts)))
   (did-update [_ _ _]
               (let [{:keys [width height]} (:div cursor)]
@@ -176,5 +182,43 @@
                   (while (.hasChildNodes n)
                     (.removeChild n (.-lastChild n))))
                 (when-let [data (seq (:data cursor))]
-                  (.log js/console (pr-str "[draw-fn]" draw-fn "[fn?]" (fn? draw-fn)))
                   ((chart-fn draw-fn) data (:div cursor) opts)))))
+
+(defn- flatten-agg-data
+  [agg-view agg-data]
+  (map (fn [data]
+         (if (map? (get data agg-view))
+           (assoc data agg-view (get-in data [agg-view :value]))
+           data))
+       agg-data))
+
+(defn- get-agg-buckets
+  [cursor agg-key agg-top]
+  (-> cursor (get-in [(keyword agg-key) :aggregations (keyword agg-top) :buckets]) seq))
+
+(defcomponent es-chart [cursor owner {:keys [id chart draw-fn agg-key agg-top agg-view] :as opts}]
+  (will-mount [_]
+              ;; Add event listener that will update width & height when window is resized
+              (.addEventListener js/window
+                                 "resize" (fn []
+                                            (let [{:keys [width height]} (get-div-dimensions id)]
+                                              (om/update! cursor :div {:width width :height height})))))
+  (render [_]
+          (let [{:keys [width height]} (:div cursor)]
+            (html [:div {:id id :width width :height height}])))
+  (did-mount [_]
+             (when-let [data (get-agg-buckets cursor agg-key agg-top)]
+               ((chart-fn draw-fn) (flatten-agg-data agg-view data) (:div cursor) opts)))
+  (did-update [_ _ _]
+              (let [{:keys [width height]} (:div cursor)]
+                (let [n (.getElementById js/document id)]
+                  (while (.hasChildNodes n)
+                    (.removeChild n (.-lastChild n))))
+                (when-let [data (get-agg-buckets cursor agg-key agg-top)]
+                  (let [flattened (flatten-agg-data agg-view data)]
+                    (if (= agg-key "TIME AGG")
+                      (do
+                        (.log js/console (pr-str agg-key))
+                        (.log js/console (pr-str flattened))
+                        ))
+                    ((chart-fn draw-fn) flattened (:div cursor) opts))))))

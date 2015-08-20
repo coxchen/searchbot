13fccee87ebbfd3af8ac56bd8b59645954b6593e
Cox Chen <coxchen@gmail.com>
2015-08-20 15:02:45 +0800

    prepare for 0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject searchbot "0.2.3"
+(defproject searchbot "0.2.4"
   :description "Searchbot for ES"
   :url "http://github.com/coxchen/searchbot"
   :license {:name "Eclipse Public License"

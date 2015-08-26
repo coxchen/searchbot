e34421a5e8d4ec13eda1183cf2c1e216e8c2fb0e
Cox Chen <coxchen@gmail.com>
2015-08-26 17:24:01 +0800

    show pie charts of related dimensions

--- src/cljs/searchbot/parsets.cljs
+++ src/cljs/searchbot/parsets.cljs
@@ -5,7 +5,8 @@
             [sablono.core :as html :refer-macros [html]]
             [cljs.core.async :refer [put! <! >! chan timeout close!]]
             [searchbot.es :refer [es-agg filtered-agg]]
-            [searchbot.input.labels :as labels]))
+            [searchbot.input.labels :as labels]
+            [searchbot.charts :refer [es-chart]]))
 
 (defn- build-sub-aggs [sub-aggs]
   (if sub-aggs {:aggs sub-aggs} {}))
@@ -19,13 +20,21 @@
   [terms sub-aggs]
   (reduce (fn [agg-query a-term]
             {:aggs (merge
-                    {(keyword a-term) (merge
-                                       {:terms (merge
-                                                {:field a-term}
-                                                (sort-sub sub-aggs))}
-                                       agg-query)}
+                    {(keyword a-term)
+                     (merge {:terms
+                             (merge {:field a-term}
+                                    (sort-sub sub-aggs))}
+                            agg-query)}
                     sub-aggs)})
           (build-sub-aggs sub-aggs) terms))
+
+(defn- build-terms-agg [prefix terms sub-aggs]
+  (let [terms-aggs (for [term terms]
+                     {(str prefix (name term)) (merge {:terms (merge {:field (name term)}
+                                                                     (sort-sub sub-aggs))}
+                                                      (build-sub-aggs sub-aggs))})
+        terms-aggs (reduce merge terms-aggs)]
+    terms-aggs))
 
 (defn- walk-buckets [de-buck bucket prefix steps value-path]
   (map #(de-buck % de-buck prefix (first steps) (rest steps) value-path)
@@ -35,30 +44,38 @@
   (if (= 0 (count steps))
     (merge prefix {current-step (:key bucket) :value (get-in bucket value-path)})
     (let [prefix (merge prefix {current-step (:key bucket)})]
-      (walk-buckets <-buckets bucket prefix steps value-path))))
+      (walk-buckets de-buck bucket prefix steps value-path))))
 
-(defn- agg->parsets [agg-result all-steps value-path]
-  (vec (flatten (walk-buckets <-buckets agg-result {} all-steps value-path))))
-
-(defn- new-svg [graph-id w h]
-  (-> js/d3 (.select graph-id) (.append "svg") (.attr "width" w) (.attr "height" h)))
+(defn- agg->parsets [agg-result steps value-path]
+  (vec (flatten (walk-buckets <-buckets agg-result {} steps value-path))))
 
 (defn- ->query
-  [{:keys [agg-terms sub-aggs filters default-filter]}]
+  [{:keys [agg-terms sub-aggs agg-prefix-terms show-pies? filters default-filter]}]
   (let [agg-query (build-parsets-agg (reverse agg-terms) sub-aggs)
+        agg-query (if (show-pies?)
+                    {:aggs (merge (:aggs agg-query)
+                                  (build-terms-agg agg-prefix-terms agg-terms sub-aggs))}
+                    agg-query)
         agg-query (filtered-agg (merge {:body agg-query}
                                  (merge {:filter default-filter}
                                         (:combined-query filters))))]
     agg-query))
 
+(defn- update-terms-aggs [owner terms-aggs]
+  (om/update-state! owner :parsets-cursor #(assoc-in % [:terms-aggs :aggregations] terms-aggs)))
+
 (defn- do-parsets-agg
   [owner]
-  (let [{:keys [continue? comm url get-agg-terms make-agg-query value-path]} (om/get-state owner)
+  (let [{:keys [continue? comm url agg-prefix-terms get-agg-terms make-agg-query value-path]} (om/get-state owner)
         {es-settings :es-settings} (om/get-shared owner)]
     (when continue?
       (go (.log js/console "# running parsets aggregation:" (pr-str (get-agg-terms)))
           (let [{agg-result :aggregations} (<! (es-agg (or url (:url-agg (es-settings))) (make-agg-query)))]
-            (>! comm (agg->parsets agg-result (get-agg-terms) value-path)))))))
+            (let [term-keys (->> (get-agg-terms) (map #(->> % name (str agg-prefix-terms) keyword)))
+                  terms-aggs (-> agg-result (select-keys term-keys))]
+              (update-terms-aggs owner terms-aggs))
+            (>! comm (agg->parsets agg-result (get-agg-terms) value-path))
+            )))))
 
 (defn- update-agg-terms-with-label
   [owner label-string]
@@ -70,51 +87,84 @@
     (do-parsets-agg owner)
     ))
 
-(defn- make-parsets-chart [{:keys [height width get-parsets-agg]}]
+(defn- new-svg [{:keys [id height width-fn]}]
+  (-> js/d3 (.select id) (.append "svg") (.attr "width" (width-fn)) (.attr "height" height)))
+
+(defn- make-parsets-chart [{:keys [height width-fn get-parsets-agg]}]
   (-> js/d3
       .parsets
       (.tension 0.8)
-      (.width width)
+      (.width (width-fn))
       (.height height)
       (.dimensions (clj->js (map name (get-parsets-agg))))
       (.value (fn [d i] (. d -value)))))
 
 (defcomponent parsets [cursor owner {:keys [agg value-path url height] :or {height 600 value-path ["doc_count"]} :as opts}]
   (init-state [_]
-              (let [{:keys [es-settings ref-state]} (om/get-shared owner)]
+              (let [{:keys [es-settings ref-state]} (om/get-shared owner)
+                    prefix-terms "terms_"]
                 {:continue? true
                  :comm (chan)
                  :url url
-                 :parsets-agg (map keyword (:terms agg))
+                 :agg-prefix-terms prefix-terms
+                 :parsets-agg (->> agg :terms (map keyword))
                  :get-agg-terms #(:parsets-agg (om/get-state owner))
-                 :make-agg-query #(->query {:agg-terms ((:get-agg-terms (om/get-state owner)))
+                 :make-agg-query #(->query {:agg-prefix-terms prefix-terms
+                                            :agg-terms ((:get-agg-terms (om/get-state owner)))
                                             :sub-aggs (:sub agg)
+                                            :show-pies? (:show-pies? (om/get-state owner))
                                             :filters (ref-state :filters)
                                             :default-filter (get-in (es-settings) [:default :filter])})
                  :value-path (apply vector (map keyword value-path))
                  :combined-query nil
-                 :svg nil}))
+                 :svg nil
+                 :parsets-cursor (om/root-cursor (atom {:div {:width "90%" :height 200}}))
+                 :show-pies (:show-pies opts)
+                 :show-pies? #(:show-pies (om/get-state owner))}))
   (will-mount [_]
               (do-parsets-agg owner))
-  (render [_]
-          (html
-           [:.card {:ref "parsets-card"}
-            [:.card-content
-             [:div.right {:style {:max-width "50%"}}
-              (let [get-agg-terms #(:parsets-agg (om/get-state owner))
-                    label-string (->> (get-agg-terms)
-                                      (map name)
-                                      (clojure.string/join " "))]
-                (om/build labels/labels cursor
-                          {:opts {:labels (labels/labelize label-string)
-                                  :on-label-updated! (partial update-agg-terms-with-label owner)}}))]
-             [:span.card-title.black-text
-              "# PARSETS [ "
-              [:strong (->> (:parsets-agg (om/get-state owner))
-                            (map name)
-                            (clojure.string/join " > "))]
-              " ]"]
-             [:div#parsets {:ref "parsets-div"}]]]))
+  (render-state [_ {:keys [parsets-agg get-agg-terms agg-prefix-terms parsets-cursor show-pies ]}]
+                (html
+                 [:.card {:ref "parsets-card"}
+                  [:.card-content
+                   [:.row
+                    [:span.card-title.black-text "# PARSETS [ " [:strong (->> parsets-agg (map name) (clojure.string/join " > "))] " ]"]
+                    [:p
+                     [:input {:type "checkbox" :class "filled-in" :checked show-pies
+                              :id "toggle-show-pies" :ref "toggle-show-pies"
+                              :on-change (fn [_] (let [thisNode (om/get-node owner "toggle-show-pies")
+                                                       checked (.-checked thisNode)]
+                                                   (om/set-state! owner :show-pies checked)
+                                                   (do-parsets-agg owner)))}]
+                     [:label {:for "toggle-show-pies"} "Show Pie Charts"]]
+                    [:div.right {:style {:max-width "50%"}}
+                     (let [label-string (->> (get-agg-terms) (map name) (clojure.string/join " "))]
+                       (om/build labels/labels cursor
+                                 {:opts {:labels (labels/labelize label-string)
+                                         :on-label-updated! (partial update-agg-terms-with-label owner)}}))]
+                    ]
+                   [:.row
+                    [:.col {:class (if show-pies "s9" "s12") :id "parsets" :ref "parsets-div"}]
+                    [:.col {:class (if show-pies "s3" "hide")}
+                     (for [term (get-agg-terms)]
+                       (let [term-name (name term)]
+                         (om/build es-chart
+                                   parsets-cursor
+                                   {:opts {:type "es-chart"
+                                           :id (str term-name "_pie")
+                                           :agg-key "terms-aggs"
+                                           :agg-top (str agg-prefix-terms term-name)
+                                           :agg-view ["key" "sum_usage"]
+                                           :draw-fn "draw-ring"
+                                           :chart {:bounds {:x "5%" :y "15%" :width "80%" :height "80%"}
+                                                   :plot "pie"
+                                                   :p-axis "sum_usage"
+                                                   :c-axis "key"}}}
+                                   )
+                         ))
+                     ]
+                    ]]]
+                 ))
   (did-update [_ _ _]
               (if-let [filters ((:ref-state (om/get-shared owner)) :filters)]
                 (let [local-combined-query (-> (om/get-state owner) :combined-query)
@@ -127,10 +177,10 @@
   (did-mount [_]
              (let [{:keys [comm parsets-agg]} (om/get-state owner)
                    parsets-div (om/get-node owner "parsets-div")
-                   card-width (.-offsetWidth parsets-div)
-                   get-svg #(new-svg "#parsets" card-width height)
+                   card-width #(.-offsetWidth parsets-div)
+                   get-svg #(new-svg {:id "#parsets" :height height :width (card-width) :width-fn card-width})
                    chart #(make-parsets-chart
-                           {:height height :width card-width
+                           {:height height :width (card-width) :width-fn card-width
                             :get-parsets-agg (fn [_] (:parsets-agg (om/get-state owner)))})]
                (.attach js/Waves (om/get-node owner "parsets-div") "waves-yellow")
                (go (while (:continue? (om/get-state owner))
